a960e3318480477f7892e437f90b708e6d0643c6
Harrison Rouillard <hrou0003@student.monash.edu>
2022-07-03 12:17:17 +1000

    added animated skills

--- pages/components/Experience.tsx
+++ pages/components/Experience.tsx
@@ -1,16 +1,17 @@
 import { motion } from "framer-motion";
 import AnimatedText from "../components/AnimatedText";
+import Skill from "./Skills/Skill";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaintBrush } from "@fortawesome/free-solid-svg-icons";
 
 const Experience = () => {
 
     return (
         <div id="experience" className="flex w-screen min-h-screen bg-gray-600">
-            <div className="m-auto border-1 rounded-md border-white p-2 text-center">
-            <AnimatedText text="Hi, my name is Harrison" className="text-6xltext-slate-300 mb-6"/>
-                <p className="text-slate-400 text-3xl w-2/3 mx-auto">
-                    I'm a recent graduated in applied mathematics and computer science from Monash University currently working
-                    at Bank of Queensland as a Data Analyst. I am looking to move into the field of software engineering.
-                </p>
+            <div className="m-auto">
+                <Skill width={0.8} label={<FontAwesomeIcon icon={faPaintBrush} fontSize="1.5rem" color="white"/>}/>
+                <Skill width={1}/>
+                <Skill width={0.6}/>
             </div>
         </div>
     )
--- pages/components/Skills/Skill.tsx
+++ pages/components/Skills/Skill.tsx
@@ -1,2 +1,26 @@
 
+import { motion } from "framer-motion"
 
+type Props = {
+    width: number;
+    label: JSX.Element;
+}
+
+const Skill: React.FC<Props> = (props) => {
+
+    return (
+        <motion.div className="flex text-center" whileHover={{ scale: 1.2, transition: { duration: 0.5 } }}>
+            <div className="pr-4 py-1">
+                {props.label}
+            </div>
+            <motion.svg height="50" width="500" className="stroke-slate-200 stroke-12" >
+                <line x1="15" y1="15" x2="415" y2="15" stroke-width="30px" className="stroke-slate-500" strokeLinecap="round" />
+                <motion.line x1="15" y1="15" x2="415" y2="15" stroke-width="30px" strokeLinecap="round" initial={{ pathLength: 0 }}
+                    animate={{ pathLength: props.width }} transition={{ duration: 1 }} />
+            </motion.svg>
+
+        </motion.div>
+    )
+}
+
+export default Skill;
