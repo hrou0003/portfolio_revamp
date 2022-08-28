56785c0101f874d20f9a36ab66ee0019ca50be46
Harrison Rouillard <hrou0003@student.monash.edu>
2022-08-28 22:45:47 +1000

    tabbed

---
+++ components/Tabbed.tsx
@@ -0,0 +1,68 @@
+import "./styles.css";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export interface Ingredient {
+  icon: string;
+  label: string;
+}
+
+export const allIngredients = [
+  { icon: "🍅", label: "Tomato" },
+  { icon: "🥬", label: "Lettuce" },
+  { icon: "🧀", label: "Cheese" },
+  { icon: "🥕", label: "Carrot" },
+  { icon: "🍌", label: "Banana" },
+  { icon: "🫐", label: "Blueberries" },
+  { icon: "🥂", label: "Champers?" }
+];
+
+const [tomato, lettuce, cheese] = allIngredients;
+export const initialTabs = [tomato, lettuce, cheese];
+
+export function getNextIngredient(
+  ingredients: Ingredient[]
+): Ingredient | undefined {
+  const existing = new Set(ingredients);
+  return allIngredients.find((ingredient) => !existing.has(ingredient));
+}
+
+const tabs = allIngredients
+
+export default function Tabbed() {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  return (
+    <div className="window">
+      <nav>
+        <ul>
+          {tabs.map((item) => (
+            <li
+              key={item.label}
+              className={item === selectedTab ? "selected" : ""}
+              onClick={() => setSelectedTab(item)}
+            >
+              {`${item.icon} ${item.label}`}
+              {item === selectedTab ? (
+                <motion.div className="underline" layoutId="underline" />
+              ) : null}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main>
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            key={selectedTab ? selectedTab.label : "empty"}
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -10, opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            {selectedTab ? selectedTab.icon : "😋"}
+          </motion.div>
+        </AnimatePresence>
+      </main>
+    </div>
+  );
+}
